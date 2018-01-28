9a7c7638efb9aa794d9969fc179a7c394b9a0cf2
Kenan Klisura <kklisura@hotmail.com>
2018-01-28 15:04:33 +0100

    Add license to header files

--- cdtp-java-client/src/main/java/com/github/kklisura/cdtp/protocol/types/network/ResourceTiming.java
+++ cdtp-java-client/src/main/java/com/github/kklisura/cdtp/protocol/types/network/ResourceTiming.java
@@ -1,5 +1,25 @@
 package com.github.kklisura.cdtp.protocol.types.network;
 
+/*-
+ * #%L
+ * cdpt-java-client
+ * %%
+ * Copyright (C) 2018 Kenan Klisura
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.github.kklisura.cdtp.protocol.support.annotations.Experimental;
 
 /** Timing information for the request. */
