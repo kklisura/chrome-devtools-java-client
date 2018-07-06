73676a096b289ebb809023a82188469aeb4135a2
Klisura, Kenan <kklisura@hotmail.com>
2018-07-06 23:21:24 +0200

    Use single thread for executing events insted of cached thread pool

--- cdt-java-client/src/main/java/com/github/kklisura/cdt/services/executors/DefaultEventExecutorService.java
+++ cdt-java-client/src/main/java/com/github/kklisura/cdt/services/executors/DefaultEventExecutorService.java
@@ -29,7 +29,7 @@ import java.util.concurrent.Executors;
  * @author Kenan Klisura
  */
 public class DefaultEventExecutorService implements EventExecutorService {
-  private ExecutorService executorService = Executors.newCachedThreadPool();
+  private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
   @Override
   public void execute(Runnable runnable) {
