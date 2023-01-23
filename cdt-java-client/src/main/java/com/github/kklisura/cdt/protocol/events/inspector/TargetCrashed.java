6fd9b1fcebbec68357948d414dc4db2c667b2dcb
Réda Housni Alaoui <reda-alaoui@hey.com>
2023-01-23 11:16:47 +0100

    Jakarta namespace migration

--- cdt-java-client/src/main/java/com/github/kklisura/cdt/protocol/events/inspector/TargetCrashed.java
+++ cdt-java-client/src/main/java/com/github/kklisura/cdt/protocol/events/inspector/TargetCrashed.java
@@ -4,7 +4,7 @@ package com.github.kklisura.cdt.protocol.events.inspector;
  * #%L
  * cdt-java-client
  * %%
- * Copyright (C) 2018 - 2021 Kenan Klisura
+ * Copyright (C) 2018 - 2023 Kenan Klisura
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
